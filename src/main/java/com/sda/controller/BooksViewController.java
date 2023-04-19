f3750a97b72b0ee2ecdd6f6dc0849a5d9eb7c3a2
BartekLabrync <barteklabrync7@gmail.com>
2023-04-19 21:25:45 +0200

    four commit

--- src/main/java/com/sda/controller/BooksViewController.java
+++ src/main/java/com/sda/controller/BooksViewController.java
@@ -1,9 +1,16 @@
 package com.sda.controller;
 
 import com.sda.model.Book;
+import com.sda.model.MenuOption;
+import com.sda.model.User;
 import com.sda.provider.BookFileProvider;
+import com.sda.util.DisplayUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BooksViewController {
     BookFileProvider bookFileProvider;
@@ -12,7 +19,19 @@ public class BooksViewController {
         bookFileProvider = new BookFileProvider();
     }
 
-    public Set<Book> getBooks() {
-        return bookFileProvider.getAllBooks();
+    public Map<Integer, String> getBooks() {
+        Set<Book> books = bookFileProvider.getAllBooks();
+        List<String> authorsAndTitles = new ArrayList<>();
+
+        for (Book book : books) {
+            authorsAndTitles.add(book.getAuthor() + ", \"" + book.getTitle() + '\"' );
+        }
+
+//        List<String> listOfBooks = bookFileProvider.getAllBooks().stream()
+//                .map(book -> (book.getAuthor() + "\t\"" + book.getTitle() + '\"'))
+//                .collect(Collectors.toList());
+
+        return new DisplayUtil<String>().convertToMap(authorsAndTitles);
+
     }
 }
