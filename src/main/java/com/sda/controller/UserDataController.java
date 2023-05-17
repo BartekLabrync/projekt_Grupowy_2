50f8499cfdfc81cf43d682928d958647508d7ecd
BartekLabrync <barteklabrync7@gmail.com>
2023-05-17 20:00:36 +0200

    Update 17.05.2023

---
+++ src/main/java/com/sda/controller/UserDataController.java
@@ -0,0 +1,43 @@
+package com.sda.controller;
+
+import com.sda.model.User;
+import com.sda.provider.UserFileProvider;
+import com.sda.provider.UserProvider;
+import com.sda.view.MainMenuView;
+import com.sda.view.View;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserDataController {
+    private UserProvider userProvider;
+
+    public Set<String> getAllUserLogins() {
+        // pobrać set<user> z userProvider
+        //przerobić na set<string> z loginami
+        // return set z loginami
+
+        Set<User> users = userProvider.getAllUsers();
+        Set<String> userLogins = users
+                .stream()
+                .map(User::getLogin)
+                .collect(Collectors.toSet());
+        return userLogins;
+    }
+
+    public Optional<User> getUser(String login){
+        // chcemy pobrać usera o danym loginie
+        return userProvider.getAllUsers().stream()
+                .filter(user -> user.getLogin().equals(login))
+                .findFirst();
+    }
+
+    public UserDataController() {
+        this.userProvider = new UserFileProvider();
+    }
+
+    public View returnMainMenu(User loggedUser) {
+        return new MainMenuView(loggedUser);
+    }
+}
--- src/main/java/com/sda/view/UserDataView.java
+++ src/main/java/com/sda/view/UserDataView.java
@@ -1,8 +1,49 @@
 package com.sda.view;
 
+import com.sda.controller.UserDataController;
+import com.sda.model.Role;
+import com.sda.model.User;
+
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.Set;
+
 public class UserDataView implements View {
+
+    private UserDataController userDataController;
+    private final Scanner scanner;
+    private final User loggedUser;
+
+    public UserDataView(User loggedUser) {
+        this.loggedUser = loggedUser;
+        this.userDataController = new UserDataController();
+        scanner = new Scanner(System.in);
+    }
+
+
     @Override
     public void display() {
-        System.out.println("UserData");
+        if(!isAdmin()){
+            userDataController.returnMainMenu(loggedUser).display();
+        }
+
+        System.out.println("Użytkownicy: ");
+        Set<String> logins = userDataController.getAllUserLogins();
+        logins.forEach(System.out::println);
+
+        System.out.println("Podaj dane Użytkownia: ");
+        String userLogin = scanner.nextLine();
+
+        //User user = userDataController.getUser(userLogin).get();
+        //System.out.println(user);
+        Optional<User> user = userDataController.getUser(userLogin);
+        if (user.isPresent()) {
+            System.out.println(user.get());
+        } else {
+            System.out.println("Użytkownik nie istnieje.");
+        }
+
+        userDataController.returnMainMenu(loggedUser).display();
+
     }
 }
